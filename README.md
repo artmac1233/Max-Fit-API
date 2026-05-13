59ddce69414c729390776b43dca134842fcd5470
arthur <artmac1233@gmail.com>
2026-05-12 23:01:21 -0300

    Implementando DELETE e UPDATE para produtos

--- README.md
+++ README.md
@@ -5,34 +5,40 @@ A API MaxFit é uma aplicação REST desenvolvida em Python com Flask, voltada p
 O projeto permite realizar operações básicas de CRUD em produtos, com integração a banco de dados MySQL, cujo modelo e estrutura foram desenvolvidos por mim, incluindo definição de tabelas, relacionamentos e constraints.
 
 ### ⚙️ Funcionalidades
+
 - Listagem de produtos (GET /produtos)
 - Cadastro de novos produtos (POST /produtos)
 - Validação de dados de entrada
 - Integração com banco de dados relacional (MySQL)
 
 ### 🧪 Testes
+
 - Testes de requisições utilizando Postman
 - Validação de respostas e códigos de status HTTP
 
 ### 🛠️ Tecnologias utilizadas
+
 - Python
 - Flask
 - MySQL
 - Postman
 
 ### 🎯 Objetivo
+
 O objetivo do projeto é praticar o desenvolvimento de APIs REST, modelagem de banco de dados e testes de API, consolidando conhecimentos em backend e QA.
 
 ### 🚀 Próximos passos
+
 - Implementar rotas de atualização (PUT), exclusão (DELETE) para produtos e finalizar todas as rotas para categoria.
 - Adicionar filtros e paginação
 - Realizar deploy da aplicação
 
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+---
 
 ## ▶️ Como executar o projeto
 
 ### 1. Clonar o repositório
+
 ```bash
 git clone https://github.com/artmac1233/Max-Fit-API.git
 cd Max-Fit-API
@@ -68,3 +74,4 @@ O projeto já contém o arquivo schema.sql com toda a estrutura do banco na past
 python app.py
 
 A API estará disponível em: http://127.0.0.1:5000
+```
--- routes/produtos.py
+++ routes/produtos.py
@@ -15,7 +15,7 @@ def listar_produtos():
 
     try:    
         cursor.execute('''
-                SELECT id, nome, marca, cstoque, categoria_ID, preco
+                SELECT id, nome, marca, estoque, categoria_ID, preco
                 FROM produtos
             ''')
 
@@ -29,6 +29,8 @@ def listar_produtos():
         con.close()
 
     return jsonify(produtos)
+
+#Método POST para criação de um novo produto
 
 @produtos_bp.route('/produtos', methods=['POST'])
 def criar_produto():
@@ -75,6 +77,148 @@ def criar_produto():
             'Detalhe': str(e)
         }), 500
 
+    finally:
+        cursor.close()
+        con.close()
+
+#Método PUT para atualizar um protudo
+
+@produtos_bp.route('/produtos/<int:id>', methods=['PUT'])
+def atualizar_produto(id):
+
+    data = request.get_json()
+
+    if not data:
+        return jsonify({
+            'Error': 'Nenhum dado enviado.'
+        }), 400
+
+    con = conectar()
+
+    if not con:
+        return jsonify({
+            'Error': 'Erro ao conectar ao banco.'
+        }), 500
+
+    cursor = con.cursor(dictionary=True)
+
+    try:
+
+        cursor.execute(
+            'SELECT * FROM produtos WHERE ID = %s',
+            (id,)
+        )
+
+        produto = cursor.fetchone()
+
+        if not produto:
+            return jsonify({
+                'Error': 'Produto não encontrado.'
+            }), 404
+
+        if 'categoria_id' in data:
+
+            cursor.execute(
+                'SELECT 1 FROM Categoria WHERE ID = %s',
+                (data['categoria_id'],)
+            )
+
+            if not cursor.fetchone():
+                return jsonify({
+                    'Error': 'Categoria não encontrada.'
+                }), 404
+
+        produto_atualizado = {
+            'nome': data.get('nome', produto['Nome']),
+            'marca': data.get('marca', produto['Marca']),
+            'preco': data.get('preco', produto['Preco']),
+            'estoque': data.get('estoque', produto['Estoque']),
+            'categoria_id': data.get('categoria_id', produto['Categoria_ID'])
+        }
+
+        sql = '''
+            UPDATE produtos
+            SET
+                Nome = %s,
+                Marca = %s,
+                Preco = %s,
+                Estoque = %s,
+                Categoria_ID = %s
+            WHERE ID = %s
+        '''
+
+        cursor.execute(sql, (
+            produto_atualizado['nome'],
+            produto_atualizado['marca'],
+            produto_atualizado['preco'],
+            produto_atualizado['estoque'],
+            produto_atualizado['categoria_id'],
+            id
+        ))
+
+        con.commit()
+
+        return jsonify({
+            'Mensagem': 'Produto atualizado com sucesso!'
+        }), 200
+
+    except Exception as e:
+
+        return jsonify({
+            'Error': 'Erro ao atualizar produto.',
+            'Detalhe': str(e)
+        }), 500
+
+    finally:
+        cursor.close()
+        con.close()
+
+#Método DELETE para deletar um produto
+
+@produtos_bp.route('/produtos/<int:id>', methods=['DELETE'])
+def deletar_produtos(id):
+
+    con=conectar()
+    
+    if not con:
+        return jsonify({
+            'Error': 'Erro ao conectar ao banco.'
+        }), 500
+    
+    cursor=con.cursor(dictionary=True)
+    
+    try:
+        cursor.execute(
+            'SELECT * FROM produtos WHERE ID = %s',
+            (id,)
+        ) 
+
+        produto=cursor.fetchone()
+
+        if not produto:
+            return jsonify({
+                'Error':'Produto inexistente',
+            }), 404
+
+        cursor.execute(
+            'DELETE FROM produtos WHERE ID = %s',
+            (id,)
+        ) 
+
+        produto = cursor.fetchone()
+
+        con.commit()
+
+        return jsonify({
+            'Mensagem': 'Produto deletado com sucesso!'
+        }), 200
+
+    except Exception as e:
+        return jsonify({
+            'Error': 'Erro ao deletar o produto.',
+            'Detalhe': str(e)
+        }), 500
+    
     finally:
         cursor.close()
         con.close()
